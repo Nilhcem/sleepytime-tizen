3daf377db88c125f8a4ee9e0ceb6250f918662f8
Gautier Nilhcem <nilhcem@gmail.com>
2016-04-30 21:01:38 +0200

    Use JQuery

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
-(function () {
+initL10N();
+$(function () {
     window.addEventListener('tizenhwkey', function (ev) {
         if (ev['keyName'] === "back") {
             try {
@@ -7,9 +8,8 @@
             catch (ignore) { }
         }
     });
-    initL10N();
-}());
+});
 function initL10N() {
     ['when_to_get_up', 'zzz', 'or', 'wake_up_at', 'calculate', 'hour', 'minute', 'am', 'pm']
-        .forEach(function (it) { document.getElementById(it).innerHTML = TIZEN_L10N[it]; });
+        .forEach(function (it) { $('#' + it).html(TIZEN_L10N[it]); });
 }
--- typescript/main.ts
+++ typescript/main.ts
@@ -1,19 +1,26 @@
+interface JQuery {
+    html(val: string): JQuery;
+}
+declare var $: {
+    (selector: string): JQuery;
+    (readyCallback: () => void ): JQuery;
+};
+
 declare var tizen: any;
 declare var TIZEN_L10N: Array<string>;
 
-(function () {
-    window.addEventListener('tizenhwkey', ev => {
+initL10N();
+$(() => {
+        window.addEventListener('tizenhwkey', ev => {
         if (ev['keyName'] === "back") {
             try {
                 tizen.application.getCurrentApplication().exit();
             } catch (ignore) { }
         }
     });
-
-    initL10N();
-} ());
+});
 
 function initL10N() {
     ['when_to_get_up', 'zzz', 'or', 'wake_up_at', 'calculate', 'hour', 'minute', 'am', 'pm']
-        .forEach(it => { document.getElementById(it).innerHTML = TIZEN_L10N[it] })
+        .forEach(it => { $('#' + it).html(TIZEN_L10N[it]) })
 }
