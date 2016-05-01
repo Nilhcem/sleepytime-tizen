d217bc4a17d8ea287cb45014cc3ea1bbf7841442
Gautier Nilhcem <nilhcem@gmail.com>
2016-05-01 10:26:10 +0200

    Simple check when value are not filled in

--- typescript/main.ts
+++ typescript/main.ts
@@ -2,18 +2,23 @@ declare var tizen: any;
 declare var TIZEN_L10N: Array<string>;
 
 interface Zepto {
-    html(val: string): Zepto;
     click(c: any)
+    html(val: string): Zepto;
+    val(): string
 }
 declare var $: {
     (selector: string): Zepto;
 };
 
+class SleepAt {
+    constructor(public hour: number, public minute: number, public amTime: boolean) { }
+}
+
 (function () {
     initL10N();
     window.addEventListener('tizenhwkey', ev => {
         if (ev['keyName'] === "back") {
-            let page = $('.ui-page-active')[0],
+            const page = $('.ui-page-active')[0],
                 pageid = page ? page.id : "";
 
             if (pageid === "main") {
@@ -27,6 +32,19 @@ declare var $: {
     });
 
     $('#zzz').click(e => { window.location.href = '#three' });
+
+    $('#calculate').click(e => {
+        const hour = parseInt($('#select-custom-0').val());
+        const minute = parseInt($('#select-custom-1').val());
+        const amTime = $('#select-custom-2').val() == "am";
+
+        if (hour == 0 || minute == 0) {
+            alert("no way");
+        } else {
+            const sleepAt = new SleepAt(hour, minute, amTime);
+            window.location.href = '#two';
+        }
+    });
 } ());
 
 function initL10N() {
