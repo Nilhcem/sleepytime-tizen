cf47230b2e6d3e1c34ab598d3ab8ff7a60b7129d
Gautier Nilhcem <nilhcem@gmail.com>
2016-05-01 09:54:14 +0200

    Add zeptojs + draft pages

--- typescript/main.ts
+++ typescript/main.ts
@@ -1,24 +1,33 @@
-interface JQuery {
-    html(val: string): JQuery;
-}
-declare var $: {
-    (selector: string): JQuery;
-    (readyCallback: () => void ): JQuery;
-};
-
 declare var tizen: any;
 declare var TIZEN_L10N: Array<string>;
 
-initL10N();
-$(() => {
-        window.addEventListener('tizenhwkey', ev => {
+interface Zepto {
+    html(val: string): Zepto;
+    click(c: any)
+}
+declare var $: {
+    (selector: string): Zepto;
+};
+
+(function () {
+    initL10N();
+    window.addEventListener('tizenhwkey', ev => {
         if (ev['keyName'] === "back") {
-            try {
-                tizen.application.getCurrentApplication().exit();
-            } catch (ignore) { }
+            let page = $('.ui-page-active')[0],
+                pageid = page ? page.id : "";
+
+            if (pageid === "main") {
+                try {
+                    tizen.application.getCurrentApplication().exit();
+                } catch (ignore) { }
+            } else {
+                window.history.back();
+            }
         }
     });
-});
+
+    $('#zzz').click(e => { window.location.href = '#three' });
+} ());
 
 function initL10N() {
     ['when_to_get_up', 'zzz', 'or', 'wake_up_at', 'calculate', 'hour', 'minute', 'am', 'pm']
